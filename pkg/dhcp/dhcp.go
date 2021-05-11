ceb2ed17f37c9c049fc8adfc75d1bba46e698f6e
liz <liz@linkingthing.com>
2021-05-11 11:06:57 +0800

    change handler to api

--- pkg/dhcp/dhcp.go
+++ pkg/dhcp/dhcp.go
@@ -6,7 +6,7 @@ import (
 	"github.com/zdnscloud/gorest"
 	restresource "github.com/zdnscloud/gorest/resource"
 
-	"github.com/linkingthing/clxone-dhcp/pkg/dhcp/handler"
+	"github.com/linkingthing/clxone-dhcp/pkg/dhcp/api"
 	"github.com/linkingthing/clxone-dhcp/pkg/dhcp/resource"
 )
 
@@ -18,14 +18,14 @@ var (
 )
 
 func RegisterHandler(apiServer *gorest.Server, router gin.IRoutes) error {
-	apiServer.Schemas.MustImport(&Version, resource.Subnet{}, handler.NewSubnetHandler())
-	apiServer.Schemas.MustImport(&Version, resource.Pool{}, handler.NewPoolHandler())
-	apiServer.Schemas.MustImport(&Version, resource.PdPool{}, handler.NewPdPoolHandler())
-	apiServer.Schemas.MustImport(&Version, resource.Reservation{}, handler.NewReservationHandler())
-	apiServer.Schemas.MustImport(&Version, resource.DhcpConfig{}, handler.NewDhcpConfigHandler())
-	apiServer.Schemas.MustImport(&Version, resource.ClientClass{}, handler.NewClientClassHandler())
-	apiServer.Schemas.MustImport(&Version, resource.StaticAddress{}, handler.NewStaticAddressHandler())
-	apiServer.Schemas.MustImport(&Version, resource.PoolTemplate{}, handler.NewPoolTemplateHandler())
+	apiServer.Schemas.MustImport(&Version, resource.Subnet{}, api.NewSubnetHandler())
+	apiServer.Schemas.MustImport(&Version, resource.Pool{}, api.NewPoolHandler())
+	apiServer.Schemas.MustImport(&Version, resource.PdPool{}, api.NewPdPoolHandler())
+	apiServer.Schemas.MustImport(&Version, resource.Reservation{}, api.NewReservationHandler())
+	apiServer.Schemas.MustImport(&Version, resource.DhcpConfig{}, api.NewDhcpConfigHandler())
+	apiServer.Schemas.MustImport(&Version, resource.ClientClass{}, api.NewClientClassHandler())
+	apiServer.Schemas.MustImport(&Version, resource.StaticAddress{}, api.NewStaticAddressHandler())
+	apiServer.Schemas.MustImport(&Version, resource.PoolTemplate{}, api.NewPoolTemplateHandler())
 
 	return nil
 }
--- pkg/metric/metric.go
+++ pkg/metric/metric.go
@@ -6,7 +6,7 @@ import (
 	restresource "github.com/zdnscloud/gorest/resource"
 
 	"github.com/linkingthing/clxone-dhcp/config"
-	"github.com/linkingthing/clxone-dhcp/pkg/metric/handler"
+	"github.com/linkingthing/clxone-dhcp/pkg/metric/api"
 	"github.com/linkingthing/clxone-dhcp/pkg/metric/resource"
 )
 
@@ -17,8 +17,8 @@ var Version = restresource.APIVersion{
 
 func RegisterHandler(apiServer *gorest.Server, router gin.IRoutes) error {
 	conf := config.GetConfig()
-	apiServer.Schemas.MustImport(&Version, resource.Dhcp{}, handler.NewDhcpHandler(conf))
-	apiServer.Schemas.MustImport(&Version, resource.Node{}, handler.NewNodeHandler())
+	apiServer.Schemas.MustImport(&Version, resource.Dhcp{}, api.NewDhcpHandler(conf))
+	apiServer.Schemas.MustImport(&Version, resource.Node{}, api.NewNodeHandler())
 	_, err := handler.NewScannedSubnetHandler()
 	if err != nil {
 		return err
