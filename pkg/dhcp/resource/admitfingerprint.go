2e1d2231fbf28aac8f2795d7ed08b04ee531c5c9
yuhaijiao <yuhj@linkingthing.com>
2022-05-26 10:09:32 +0000

    fix admin client type valid

--- pkg/dhcp/resource/admitfingerprint.go
+++ pkg/dhcp/resource/admitfingerprint.go
@@ -5,6 +5,8 @@ import (
 
 	restdb "github.com/linkingthing/gorest/db"
 	restresource "github.com/linkingthing/gorest/resource"
+
+	"github.com/linkingthing/clxone-dhcp/pkg/util"
 )
 
 var TableAdmitFingerprint = restdb.ResourceDBType(&AdmitFingerprint{})
@@ -19,7 +21,7 @@ func (a AdmitFingerprint) GetParents() []restresource.ResourceKind {
 }
 
 func (a *AdmitFingerprint) Validate() error {
-	if len(a.ClientType) == 0 {
+	if len(a.ClientType) == 0 || util.ValidateStrings(a.ClientType) != nil {
 		return fmt.Errorf("admit client type is required")
 	} else {
 		return nil
