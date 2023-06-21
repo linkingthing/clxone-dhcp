96c4cd06dc81630975785de553540ef1230b3031
雷鹏 <leip@linkingthing.com>
2023-06-21 09:57:06 +0800

    optimize error prompt

--- pkg/dhcp/service/reservation4_service.go
+++ pkg/dhcp/service/reservation4_service.go
@@ -523,7 +523,7 @@ func (s *Reservation4Service) parseReservation4sFromFile(fileName string, subnet
 	response *excel.ImportResult) ([]*resource.Reservation4, error) {
 	contents, err := excel.ReadExcelFile(fileName)
 	if err != nil {
-		return nil, err
+		return nil, errorno.ErrReadFile(fileName, err.Error())
 	}
 
 	if len(contents) < 2 {
